b737744afdd4e4924e6a14846725de15fb24fb5b
JacobChwastek <jacobchwastek@gmail.com>
2018-12-12 16:02:30 +0100

    helloworld

---
+++ CoolSchool/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace CoolSchool.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            return View("Index");
+        }
+        //public string Index()
+        //{
+        //    return "Hello World";
+        //}
+    
+
+    }
+}
